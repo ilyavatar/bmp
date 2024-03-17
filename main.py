dcfb9826df5d5340eef8cb47f201c35e631bf01a
Ilya <mr.luzin.ilya@yandex.ru>
2024-03-17 17:16:21 +0300

    Добавлен web интерфейс для загрузки файла и его отображения, а также подставление расшифрованной и зашифрованной картинки

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
+import shutil
 import eel
+import wx
 
+# @eel.expose
+# def cls_func(cls):
+#     if (cls.startswith('Загрузить')):
+#         print('ЗагрузОчка')
+#     elif (cls.startswith('Сохранить')):
+#         with open('output.txt', 'w') as file:
+#             file.write('Сохранил какие-то данные')
 
 @eel.expose
-def cls_func(cls):
-    if (cls.startswith('Загрузить')):
-        print('ЗагрузОчка')
-    elif (cls.startswith('Сохранить')):
-        with open('output.txt', 'w') as file:
-            file.write('Сохранил какие-то данные')
+def downloadImage(wildcard="*"):
+    app = wx.App(None)
+    style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
+    dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
+    if dialog.ShowModal() == wx.ID_OK:
+        path = dialog.GetPath()
+        # Сделать всякие проверки (например, что это bmp файл)
+        name = dialog.GetFilename()
+        shutil.copyfile(path, "web/tmp/input.bmp")
+    else:
+        name = "error.png"
+    dialog.Destroy()
+    return [name, "input.bmp"]
+
+@eel.expose
+def saveImage(wildcard="*"):
+    app = wx.App(None)
+    style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
+    dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
+    if dialog.ShowModal() == wx.ID_OK:
+        path = dialog.GetPath()
+    else:
+        path = None
+    dialog.Destroy()
+    return path
+
+# @eel.expose
+# def encrypt(wildcard="*"):
+
+
+# @eel.expose
+# def encrypt(wildcard="*"):
 
 
 # Set web files folder
